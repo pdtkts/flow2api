3f14dfc103f3f4506e00b555da5576f0ae6f46d0
nhucuc9999 <nhucuc9999@gmail.com>
2026-05-08 06:08:08 +0700

    refactor(config): update settings and vite config
    - Adjust setting.toml configurations
    - Update frontend vite.config.ts settings

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,18 +12,18 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000',
+      "/api": {
+        target: "http://127.0.0.1:38000",
         changeOrigin: true,
       },
-      '/v1': {
-        target: 'http://127.0.0.1:8000',
+      "/v1": {
+        target: "http://127.0.0.1:38000",
         changeOrigin: true,
       },
-      '/tmp': {
-        target: 'http://127.0.0.1:8000',
+      "/tmp": {
+        target: "http://127.0.0.1:38000",
         changeOrigin: true,
       },
-    }
-  }
-})
+    },
+  },
+});
